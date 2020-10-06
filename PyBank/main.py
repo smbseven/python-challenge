d7ea6d390257216aa08f18b6deb3c5854041166b
Sergio Mejía <sergiomejia@SMBAir.local>
2020-10-05 23:32:51 -0500

    PyBank fully operational

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,4 @@
+from prettytable import PrettyTable
 import os
 import csv
 pybank_csv = os.path.join('Resources','02-Homeworks_03-Python_Instructions_PyBank_Resources_budget_data.csv')
@@ -14,7 +15,7 @@ with open(pybank_csv,'r') as csvfile:
     first_row = next(csvreader)
     Total_profit = int(first_row[1])
     prev_prof_value = int(first_row[1])
-    print(Total_profit,prev_prof_value)
+    # print(Total_profit,prev_prof_value)
     # first_row = next(csvreader)
     # print(header)
     for row in csvreader:
@@ -30,7 +31,24 @@ with open(pybank_csv,'r') as csvfile:
             greatest_dec = change
             great_d_mo = row[0]
 average_profit = sum(month_list_change)/len(month_list_change)
-print(Total_profit,month_counter,greatest_dec,great_d_mo,greatest_inc,great_i_mo,average_profit)
+aver_prof = round(average_profit,2)
+x=PrettyTable()
+x.field_names = ["Variables", "Values", "Month"]
+x.add_row(["Number of months:",month_counter,""])
+x.add_row(["Total amount of P/L:",Total_profit,""])
+x.add_row(["Average change:",aver_prof,""])
+x.add_row(["Greatest increase in profits:",greatest_inc,great_i_mo])
+x.add_row(["Greatest decreas in profits:",greatest_dec,great_d_mo])
+print(x)
+analysis_file=os.path.join("analysis","anlysis.txt")
+with open(analysis_file,"w") as f:
+    print(x,file=f)
+# print(Total_profit,month_counter,greatest_dec,great_d_mo,greatest_inc,great_i_mo,average_profit)
+# print("- - - - - - - - - - - - - - - - - - - - - - ")
+# Print("!             Financial Analysis           !")
+# Print("- - - - - - - - - - - - - - - - - - - - - - ")
+# Print("Total Months =" month_counter)
+# Print("Total =")
 
         # counter for months
 
--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # python-challenge
 Python homework
-Game Plan:
+PyBank Game Plan:
     Import CSV file
     Mind the path
-    Make a list out of each column
-    Count the amount of months (Column Date) - save the result in a variable
-    Sum the total of Proft/losses (Colum Profit/Losses) - save the result in  avariable
+    First "routine"
+    Eliminate the first row
+    Iterate through the list
     Find the largest value in Profit/Losses - save the result in a variable?
     Find the lowest value in Profit/Losses - save the result in a variable?
+    Make a list out of each column
+    Count the amount of months (Column Date) - save the result in a variable
+    Sum the total of Proft/losses (Colum Profit/Losses) - save the result in a variable
     Create a dictionary with the keys and the values (afore mentioned variables)
     Print the dictionary (on screen)
-    Export the dictionary into a CSV file in the analysis folder
+    Export the dictionary into a Text file in the analysis folder
+PyPoll Game Plan:
+
     
